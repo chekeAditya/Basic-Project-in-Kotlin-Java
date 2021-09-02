98f2f84c0ef333cd024f57a762f6bfd7983c4c47
Aditya Cheke <adityacheke4@gmail.com>
2021-09-02 22:32:20 +0530

    Introduction

---
+++ Kotlin Basics/src/Kotlin_From_Basic_To_Advanced/OOPS/A_introduction.java
@@ -0,0 +1,20 @@
+package Kotlin_From_Basic_To_Advanced.OOPS;
+
+public class A_introduction {
+
+    /**
+     * OOPS :- Object Oriented Programming System
+     * A programming paradigm that uses the concept of classes and objects.
+     * IN OOPS we had class & Objects
+     * Properties and functions
+     *
+     * Classes are the blueprint of the objects. WHere we put both variable and function
+     *
+     * Four Main Principles of OOP
+     * Encapsulation :-  it is encapsulating data and function together
+     * Inheritance :- it means we can use the property and function of one class to another class
+     * Abstraction :- It means hiding ( when we design a system we had to hide something and visible whenever it's necessary)
+     * Polymorphism :- We will design a classes in a way that we can reuse the same function to achieve different behaviour's and different classes
+     */
+
+}
