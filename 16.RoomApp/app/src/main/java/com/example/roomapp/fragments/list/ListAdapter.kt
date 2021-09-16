b451269fd6a6d575721a1451c06f68ea64f41b47
Aditya Cheke <adityacheke4@gmail.com>
2021-09-17 00:23:00 +0530

    Read Data

---
+++ 16.RoomApp/app/src/main/java/com/example/roomapp/fragments/list/ListAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.roomapp.fragments.list
+
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomapp.R
+import com.example.roomdatabase.data.User
+import kotlinx.android.synthetic.main.custom_row.view.*
+
+class ListAdapter : RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+
+    private var userList = emptyList<User>()
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.custom_row,parent,false))
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentItem = userList[position]
+        holder.itemView.tvId.text = currentItem.id.toString()
+        holder.itemView.tvFirstName.text = currentItem.firstName
+        holder.itemView.tvLastName.text = currentItem.lastName
+        holder.itemView.tvAge.text = currentItem.age.toString()
+    }
+
+    fun setData(user: List<User>){
+        this.userList = user
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+}
